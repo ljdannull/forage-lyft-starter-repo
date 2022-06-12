9958389d25a2b25988ac9a64ecddfa8e811cd176
Lukas Dannull <lukasdannull@Lukass-MBP.attlocal.net>
2022-06-11 22:37:30 -0400

    refactored

--- car/car.py
+++ car/car.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
+from car.serviceable import Serviceable
 
 
-class Car(ABC):
+class Car(Serviceable):
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
 
-    @abstractmethod
     def needs_service(self):
-        pass
+        return self.engine.needs_service() or self.battery.needs_service()
